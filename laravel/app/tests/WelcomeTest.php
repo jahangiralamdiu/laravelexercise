0948b53a1fad692fdab908ec80adf46ed10f6caf
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-08-18 15:24:24 +0600

    write test code to test a controller

---
+++ laravel/app/tests/WelcomeTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LAB5
+ * Date: 8/18/14
+ * Time: 3:13 PM
+ */
+
+class WelcomeTest extends TestCase {
+
+    public function testBasicExample()
+    {
+        $response = $this->action('GET', 'WelcomeController@index');
+
+        $this->assertEquals('Hello World', $response->getContent());
+    }
+
+}
+ 
