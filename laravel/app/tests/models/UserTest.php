6c8276507d3bed287afc7f3eeb3d88c1b04122f2
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-08-19 11:04:38 +0600

    write test code to test a controller

---
+++ laravel/app/tests/models/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LAB5
+ * Date: 8/18/14
+ * Time: 3:38 PM
+ */
+
+namespace models;
+
+class UserTest extends TestCase {
+
+    public function testThatTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+} 
